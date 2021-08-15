b0127225a48d2c5ed36a6220fc87e1c87b54c016
LRS4 <42714431+LRS4@users.noreply.github.com>
2021-08-15 14:42:15 +0100

    Add image input widget

--- lib/palette_screen.dart
+++ lib/palette_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/image_input.dart';
 
 class ShowPaletteScreen extends StatefulWidget {
   static const routeName = "/palette";
@@ -7,39 +8,48 @@ class ShowPaletteScreen extends StatefulWidget {
 }
 
 class _ShowPaletteScreenState extends State<ShowPaletteScreen> {
-  @override 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Color Extractor")
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Text("User inputs..."),
-          ElevatedButton.icon(
-            icon: Icon(Icons.camera_alt),
-            label: Text("Take Photo"),
-            onPressed: () {
-              
-            },
-            style: ButtonStyle(
-              foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-              padding: MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.zero),
-              elevation: MaterialStateProperty.all<double>(0.0),
-              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-              backgroundColor: MaterialStateProperty.all<Color>(Colors.purple.shade300),
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.zero,
-                  side: BorderSide(color: Colors.red)
+        appBar: AppBar(title: Text("Color Extractor")),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(labelText: "Title")
+                      ),
+                      SizedBox(height: 10),
+                      ImageInput()
+                    ],
+                  )
                 )
-              ) 
-            )
-          )
-        ],
-      ) 
-    );
+              )
+            ),
+            ElevatedButton.icon(
+                icon: Icon(Icons.colorize),
+                label: Text("Extract Colors"),
+                onPressed: () {},
+                style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                        EdgeInsets.zero),
+                    elevation: MaterialStateProperty.all<double>(0.0),
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        Colors.purple.shade300),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,
+                            side: BorderSide(color: Colors.red)))))
+          ],
+        ));
   }
 }
